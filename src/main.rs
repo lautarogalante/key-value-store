c350c7ebd0452cd50a95ce0a1527d2d841adf204
Lautaro Galante <lautaro9galante@gmail.com>
2025-10-15 18:38:07 -0300

    better formatting and panic case removed

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,56 @@
-use std::{env::args, io::{self, Read, Write}, net::{TcpListener, TcpStream}};
 use kv_store::kv;
+use std::{
+    env::args, io::{self, Read, Write}, net::{TcpListener, TcpStream}
+};
 
-
-fn handle_connection(stream: &mut TcpStream) ->std::io::Result<i32> {
+fn handle_connection(stream: &mut TcpStream) -> std::io::Result<i32> {
     let mut buffer = [0; 1024];
     let mut in_memory = kv::Store::new();
-    
-    loop  {
+
+    loop {
         let n = match stream.read(&mut buffer) {
             Ok(0) => {
                 let _ = io::stdout().write_all(b"Close connection\n");
                 return Ok(0);
-            },
+            }
             Ok(n) => n,
             Err(e) => {
                 println!("Error reading from buffer: {e}");
                 return Err(e);
             }
         };
-        
-        let input = String::from_utf8(buffer[..n].to_vec());
-        let value = in_memory.execute(&input.unwrap());
-        let _ = stream.write_all(format!("{:?}\n", value).as_bytes());
+
+        let input = match String::from_utf8(buffer[..n].to_vec()){
+            Ok(s) => s,
+            Err(e) => {
+                let error_msg = format!("Error: Invlid UTF-8 - {}\n", e);
+                stream.write_all(error_msg.as_bytes())?;
+                continue;
+            }
+        };
+
+        let value = in_memory.execute(&input).unwrap_or_else(|e|format!("Error: {}", e));
+        let _ = stream.write_all(format!("{}\n",value).as_bytes());
     }
 }
-
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = args().collect();
     let mut port: u16 = 8080;
     if args.len() > 1 {
         match args[1].parse::<u16>() {
-            Ok(argument ) => {
-                port = argument
-            }
+            Ok(argument) => port = argument,
             Err(e) => {
                 println!("The argument must be an integer: {e}");
-                return Ok(())
+                return Ok(());
             }
         }
     }
 
     let listener = TcpListener::bind(("127.0.0.1", port))?;
-    let _ = io::stdout().write_all(format!("Welcome to KV-Store - Listening on port: {}\n", port).as_bytes());
-    
+    let _ = io::stdout()
+        .write_all(format!("Welcome to KV-Store - Listening on port: {}\n", port).as_bytes());
+
     let mut status: i32;
 
     for stream in listener.incoming() {
@@ -54,7 +61,9 @@ fn main() -> std::io::Result<()> {
                     break;
                 }
             }
-            Err(e) => { println!("{e}")}
+            Err(e) => {
+                println!("{e}")
+            }
         }
     }
     Ok(())
